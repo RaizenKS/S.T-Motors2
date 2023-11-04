938f21eee2ffcf5f70873ae59e769e2656504368
RaizenKS <dieguinhoxx6@gmail.com>
2023-11-03 22:32:54 -0300

    First Commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const btnHome= document.querySelector("#btn-home");
+const box = document.querySelectorAll(".box")
+
+
+
+function home(){location.href="index.html"}
+btnHome.addEventListener("click", home)
