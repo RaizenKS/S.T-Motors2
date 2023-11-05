b0626751cbec167feb4361ccc54583c7d69c356e
RaizenKS <dieguinhoxx6@gmail.com>
2023-11-04 22:12:02 -0300

    agora foi

--- script/script.js
+++ script/script.js
@@ -2,7 +2,10 @@ const body = document.querySelector("body");
 const btnHome= document.querySelector("#btn-home");
 const container = document.querySelector("#container");
 const box = document.querySelectorAll(".box");
-const interface2 = document.querySelector("#interface2")
+const interface2 = document.querySelector("#interface2");
+const labelEmp= document.querySelector("#lab-emp")
+
+
 function home(){location.href="index.html"}
 
 btnHome.addEventListener("click", home)
@@ -19,9 +22,12 @@ box.forEach((nbox) =>{
         box[5].style.display = "none";
 
         interface2.style.display = "grid";
+        body.style.height = "150vh"
         container.style.gridTemplateColumns = "1fr";
-        container.style.gridTemplateRows = "0.1fr 1fr";
+        container.style.gridTemplateRows = "1fr";
         container.style.padding ="0px";
+        labelEmp.innerHTML = nbox.innerHTML
+
         
         
       
